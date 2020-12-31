c9cfa27c42b89a07f1bb696cd3389e0863b0acb1
matheustorres <matheustf2003@gmail.com>
2020-12-31 16:12:26 -0300

    Criacao do Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,16 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
-    public void PlayGame()
-
-    {
+    public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void QuitGame()
-    {
+    public void QuitGame() {
         Application.Quit();
-
     }
 }
